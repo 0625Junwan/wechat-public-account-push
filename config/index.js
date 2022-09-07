135576b401af4aca4555b9423ca74d9e796fc1d6
0625Junwan <103887676+0625Junwan@users.noreply.github.com>
2022-09-07 11:49:42 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -43,6 +43,20 @@ export const config = {
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn"
     }, 
+    {
+      // 想要发送的人的名字
+      name: "萌宝",
+      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: "oxpPO6kM5ptaysFWnE80emAEi9tw",
+      // 你想对他发送的模板消息的模板ID
+      useTemplateId: "23W-VzXLrsswuzQn2I02sSnMpVeZhW4OZ5XdAxIbBRE",
+      // 所在省份
+      province: "山西",
+      // 所在城市
+      city: "运城",
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: "https://wangxinleo.cn"
+    }, 
   ],
 
   /**
